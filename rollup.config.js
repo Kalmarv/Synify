73d48e8702aa96d8a18cae36dd5b4dedc6f3ac52
CalebPfaff <kalmarv@pm.me>
2022-05-04 20:28:29 -0600

    Let's see if Vercel builds

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import json from "@rollup/plugin-json";
+import replace from "@rollup/plugin-replace";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,6 +43,10 @@ export default {
     file: "public/build/bundle.js",
   },
   plugins: [
+    replace({
+      preventAssignment: true,
+      "http://localhost:8888": "https://three-spot.vercel.app/",
+    }),
     svelte({
       compilerOptions: {
         // enable run-time checks when not in production
