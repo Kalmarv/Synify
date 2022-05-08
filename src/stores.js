032fc34dbc2a7d31df65f7d53f00ecb22f2fee3c
Kalmarv <59708937+Kalmarv@users.noreply.github.com>
2022-05-08 17:34:00 -0600

    🐞fix: Scene updates on title change, instead of album cover change

--- src/stores.js
+++ src/stores.js
@@ -1,17 +1,14 @@
 import { readable, writable } from "svelte/store";
 
-export const code = writable(null);
-
 export const appUrl = readable(null, function start(set) {
   set("ROLLUP_REPLACE");
 });
 
-export let authToken = writable(null);
-export let refreshToken = writable(null);
-
+export const code = writable(null);
+export const authToken = writable(null);
+export const refreshToken = writable(null);
 export const clientID = readable("6888734b4fc749cdb701c89482a3db41");
-
-export let songName = writable(null);
-export let songArtist = writable(null);
-export let songImage = writable(null);
-export let songLink = writable(null);
+export const songName = writable(null);
+export const songArtist = writable(null);
+export const songImage = writable(null);
+export const songLink = writable(null);
